a5c8644f49ebe9795f1f2091b9869f3d90f2adab
Lindsey Olson <l.olson.swift@gmail.com>
2024-02-23 22:11:31 -0500

    Initial Setup

---
+++ Recipes/Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Lindsey Olson on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
